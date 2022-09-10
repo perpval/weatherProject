759f29b2ec0e66b84097384ace9c6da1694785c7
perpval <108651184+perpval@users.noreply.github.com>
2022-09-10 18:30:32 +0000

    included temperature conversions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function weather() {
+function weather(event) {
+  event.preventDefault();
   let apiKey = "b19f4df02ac912fe1dad7424873d1b77";
   let city = document.querySelector("#city-input").value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -10,8 +11,8 @@ function weatherDetails(response) {
   let currentCity = document.querySelector("#current-city");
   currentCity.innerHTML = response.data.name;
   let temperature = document.querySelector("#temperature");
-  let currentTemp = Math.round(response.data.main.temp);
-  temperature.innerHTML = currentTemp;
+  celsiusTemp = Math.round(response.data.main.temp);
+  temperature.innerHTML = celsiusTemp;
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = response.data.main.humidity;
   let speed = document.querySelector("#speed");
@@ -31,15 +32,11 @@ function weatherDetails(response) {
   );
 }
 
-let searchButton = document.querySelector("#search-button");
-searchButton.addEventListener("click", weather);
-
 function onLoading(city) {
   let apiKey = "b19f4df02ac912fe1dad7424873d1b77";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(weatherDetails);
 }
-onLoading("Accra");
 
 function getLocation() {
   navigator.geolocation.getCurrentPosition(cityCoords);
@@ -52,8 +49,35 @@ function cityCoords(position) {
   axios.get(apiUrl).then(weatherDetails);
 }
 
+function fahrenheitTemperature(event) {
+  event.preventDefault();
+  document.querySelector("#temperature").innerHTML = Math.round(
+    (celsiusTemp * 9) / 5 + 32
+  );
+  celsius.classList.remove("colorChange");
+  fahrenheit.classList.add("colorChange");
+}
+function celsiusTemperature(event) {
+  event.preventDefault();
+  document.querySelector("#temperature").innerHTML = Math.round(celsiusTemp);
+  celsius.classList.add("colorChange");
+  fahrenheit.classList.remove("colorChange");
+}
+let celsiusTemp = null;
+
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", fahrenheitTemperature);
+
+let celsius = document.querySelector("#celsius");
+celsius.addEventListener("click", celsiusTemperature);
+
 let currentButton = document.querySelector("#current-button");
 currentButton.addEventListener("click", getLocation);
+
+let searchButton = document.querySelector("#search-button");
+searchButton.addEventListener("click", weather);
+
+onLoading("Accra");
 
 function theTime(date) {
   let days = [
